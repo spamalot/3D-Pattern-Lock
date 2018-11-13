2db336e0f387858c6eb47c2c3b7750a2fdcacff0
Yixin Wang <ywang@sprypoint.com>
2018-11-13 12:04:53 -0500

    first commit

---
+++ KeyPad/Assets/TouchFollower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TouchFollower : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public Canvas canvas;
+
+	// Update is called once per frame
+	void Update () {
+        var r = canvas.GetComponent<RectTransform>();
+        Vector2 x;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(r, Input.mousePosition, null, out x);
+        Debug.Log(x - new Vector2(-240, 400));
+        GetComponent<RectTransform>().anchoredPosition = x;
+	}
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("down");
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        //Debug.Log("drag"+data.position);
+        //GetComponent<RectTransform>().position = data.position;
+    }
+
+  
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("up");
+    }
+}
---
+++ KeyPad/Assets/touchtest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchtest : MonoBehaviour {
+
+    public float leftOffset = 114;
+    public float topOffset = 458;
+    public float spacing = 127;
+
+    public Camera c;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var a = c.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(a + " "+Input.GetMouseButton(1));
+	}
+}
