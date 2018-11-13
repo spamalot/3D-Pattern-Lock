5c375708f8b42f5d9aa9a5c26c8651bcc6d0e357
unknown <mlakier@hci-vr3.cs.uwaterloo.ca>
2018-11-13 15:53:56 -0500

    Fixed renamed TouchTest file

---
+++ KeyPad/Assets/TouchTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour {
+
+	public float leftOffset = 114;
+	public float topOffset = 458;
+	public float spacing = 127;
+
+	public Camera c;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Debug.Log(Input.GetMouseButton(0));
+	}
+}
