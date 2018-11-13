05a49bb941bd9685336fb77a0deecb1fe4601aac
unknown <mlakier@hci-vr3.cs.uwaterloo.ca>
2018-11-13 15:50:42 -0500

    Added library to render lines; cleaned up old code

--- KeyPad/Assets/TouchFollower.cs
+++ KeyPad/Assets/TouchFollower.cs
@@ -1,36 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-public class TouchFollower : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public class TouchFollower : MonoBehaviour {
 
     public Canvas canvas;
 
-	// Update is called once per frame
-	void Update () {
+    // Changing this has no effect, only for reading purposes
+    public Vector2 posn;
+
+    void Update () {
         var r = canvas.GetComponent<RectTransform>();
         Vector2 x;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(r, Input.mousePosition, null, out x);
-        Debug.Log(x - new Vector2(-240, 400));
+        posn = x - new Vector2(-240, 400);
         GetComponent<RectTransform>().anchoredPosition = x;
-	}
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("down");
-    }
-
-    public void OnDrag(PointerEventData data)
-    {
-        //Debug.Log("drag"+data.position);
-        //GetComponent<RectTransform>().position = data.position;
-    }
-
-  
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("up");
     }
 }
