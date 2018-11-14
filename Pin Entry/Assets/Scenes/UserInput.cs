c0519885d5494f566f521e9250fc70a9df3b96a5
Dimcho Karakashev <dkarakas@users.noreply.github.com>
2018-11-14 15:37:46 -0500

    Key Pad Initial Implementation

---
+++ Pin Entry/Assets/Scenes/UserInput.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour {
+
+    public GameObject[] buttons;
+    public Text txtInput;
+
+    Gyroscope m_Gyro;
+    private string passwordEntered;
+    
+    // Use this for initialization
+    void Start () {
+        m_Gyro = Input.gyro;
+        m_Gyro.enabled = true;
+    }
+
+    void Awake ()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.touchCount > 0) {
+            //Touch myTouch = Input.touches[0];
+            Touch myTouch = Input.GetTouch(0);
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                Debug.Log("PIN ENTRY " + System.DateTime.Now + " : Begun " + myTouch.position + "\n");
+            }else if(myTouch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("PIN ENTRY " + System.DateTime.Now + " :  End " + myTouch.position + "\n");
+            }
+            else
+            {
+                Debug.Log("PIN ENTRY " + System.DateTime.Now + " : " + myTouch.position);
+            }
+        }
+
+        Debug.Log("Gyro rotation rate " + m_Gyro.rotationRate);
+        Debug.Log("Gyro attitude" + m_Gyro.attitude);
+        Debug.Log("Gyro enabled : " + m_Gyro.enabled);
+
+
+
+    }
+
+
+    public void OnClickBtn(int buttPressed)
+    {
+        txtInput.text = txtInput.text + "*";
+        passwordEntered = passwordEntered + buttPressed.ToString();
+        if(passwordEntered.Length == 4)
+        {
+            Debug.Log(passwordEntered);
+            passwordEntered = "";
+            txtInput.text = "";
+        }
+    }
+
+ 
+}
