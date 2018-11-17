a9086439c925e3ff03586d025883fe2fa617a989
unknown <mlakier@hci-vr3.cs.uwaterloo.ca>
2018-11-17 17:34:44 -0500

    added missing files

---
+++ KeyPad/Assets/ExperimentController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentController : MonoBehaviour {
+
+    public enum Technique { PIN, Pattern, Pattern3D };
+
+
+    public int participantId;
+    public Technique technique;
+
+    // chooses techniques
+    // chooses "random" pin
+
+    public List<string> PINPins;
+    public List<string> PatternPins;
+    public List<string> Pattern3DPins;
+
+
+}
---
+++ KeyPad/Assets/Vicon/LocalCoordThing.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class LocalCoordThing : MonoBehaviour {
+
+    public float phoneWidth = 0.8f;
+    public float pixelWidth = 480;
+    public float aspect = 800f / 480f;
+
+    public float close = 0.2f;
+    public float middle = 0.5f;
+    public float far = 1f;
+
+    //public enum Distance { Close, Middle, Far, Inactive };
+
+    //public Distance distance;
+    //public Vector2 posn;
+
+    public Vector3 posn;
+
+    void Update () {
+        var unitScale = 1.0f / phoneWidth;
+        var p = transform.localPosition;
+
+        var xyz = new Vector3((p.x * unitScale + 0.5f) * pixelWidth, aspect* pixelWidth - (p.y * unitScale + 0.5f * aspect) * pixelWidth, -p.z);
+        //posn = new Vector2(xyz.x, xyz.y);
+        /*if (xyz.z >= 0 && xyz.z < close) {
+            distance = Distance.Close;
+        } else if (xyz.z >= close && xyz.z < middle) {
+            distance = Distance.Middle;
+        } else if (xyz.z >= middle && xyz.z < far) {
+            distance = Distance.Far;
+        } else {
+            distance = Distance.Inactive;
+        }*/
+        posn = new Vector3(xyz.x, xyz.y, xyz.z * pixelWidth * unitScale);
+        Debug.Log(posn);// +" " + distance);
+    }
+}
