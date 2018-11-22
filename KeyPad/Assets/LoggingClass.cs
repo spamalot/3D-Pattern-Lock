2e0e39c280ba3cd454c1190b6b0501010e34436e
Dimcho Karakashev <dkarakas@purdue.edu>
2018-11-21 21:27:00 -0800

    Better LoggingSystem

--- KeyPad/Assets/LoggingClass.cs
+++ KeyPad/Assets/LoggingClass.cs
@@ -8,6 +8,9 @@ public static class LoggingClass
 
     public static string userId;
     public static string technique;
+    public static string pinTestedNumber;
+    public static string pinTrailNumber;
+    public static string actualPin;
 
     public static void setLoggerUser(string user)
     {
@@ -19,11 +22,38 @@ public static class LoggingClass
         technique = techniqueStarted;
     }
 
+    public static void setLoggerPinTestedNumber (string pinNumber)
+    {
+        pinTestedNumber = pinNumber;
+    }
+
+    public static void setLoggerPinTrailNumber (string pinTrailNum)
+    {
+        pinTrailNumber = pinTrailNum;
+    }
+
+    public static void setLoggerActualPin (string actualPinNumber)
+    {
+        actualPin = actualPinNumber;
+    }
+
+    /*Event logged can be any of these:
+     * Event types:
+     * - drag start
+     * - drag end
+     * - drag move
+     * - pin character entered
+     * - pin entry started
+     * - pin entry finished
+     * - vicon motion
+     * - button press
+     * */
+
     public static void appendToLog(string eventLogged,string dataToWrite)
     {
         string path = "Assets/userLogs/" + userId + "_" + technique + ".txt";
         StreamWriter writer = new StreamWriter(path, true);
-        writer.WriteLine(System.DateTime.Now + ": " + eventLogged + " " + dataToWrite);
+        writer.WriteLine(System.DateTime.Now + ", " + userId + ", " + technique + ", " + pinTestedNumber + ", " + pinTrailNumber + ", " + eventLogged + ", " + actualPin + ", " + dataToWrite);
         writer.Close();
 
     }
